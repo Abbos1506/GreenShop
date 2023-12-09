703dfa3df9c64d92d30b7b9ef1a0b9627c849d2d
Abbos1506 <abbosomonboyev686@gmail.com>
2023-12-09 14:43:53 +0500

    change footer

--- src/pages/home/companents/FooterComp.jsx
+++ src/pages/home/companents/FooterComp.jsx
@@ -1,4 +1,14 @@
 import footerCard from "../../../utils/footerCard";
+import FooterLogo from "../../../assets/image/navLogo.png";
+import FooterLoc1 from "../../../assets/icons/FooterLoc1.png";
+import FooterEmail from "../../../assets/icons/FooterEmail.png";
+import FooterCall from "../../../assets/icons/FooterCall.png";
+import {
+  footerAcc,
+  footerCate,
+  footerHelp,
+  footerSocials,
+} from "../../../utils/footerInfo";
 
 const FooterComp = () => {
   return (
@@ -20,10 +30,7 @@ const FooterComp = () => {
             Would you like to join newsletters?
           </h2>
           <div className="newsletters__input">
-            <input
-              type="email"
-              placeholder="enter your email address..."
-            />
+            <input type="email" placeholder="enter your email address..." />
             <button className="input__btn">Join</button>
           </div>
           <p className="newsletters__text">
@@ -33,6 +40,56 @@ const FooterComp = () => {
           </p>
         </div>
       </div>
+      <div className="footer__center">
+        <div className="footer__logo">
+          <img src={FooterLogo} alt="footer logo" />
+        </div>
+        <div className="footer__contacts">
+          <div className="footer__contacts__location">
+            <img src={FooterLoc1} alt="footer location icon" />
+            <p>
+              70 West Buckingham Ave. <br /> Farmingdale, NY 11735
+            </p>
+          </div>
+          <div className="footer__contacts__email">
+            <img src={FooterEmail} alt="footer email icon" />
+            <p>abbosomonboyev1506@gmail.com</p>
+          </div>
+          <div className="footer__contacts__call">
+            <img src={FooterCall} alt="footer call icon" />
+            <p>+998 33 317 17 18</p>
+          </div>
+        </div>
+      </div>
+      <div className="footer__info">
+        <div className="footer__acc">
+          <h2 className="footer__acc__title">My Account</h2>
+          {footerAcc.map((item) => (
+            <p className="footer__acc__list">{item.title}</p>
+          ))}
+        </div>
+        <div className="footer__help">
+          <h2 className="footer__help__title">Help & Guide</h2>
+          {footerHelp.map((item) => (
+            <p className="footer__help__list">{item.title}</p>
+          ))}
+        </div>
+        <div className="footer__cate">
+          <h2 className="footer__cate__title">Categories</h2>
+          {footerCate.map((item) => (
+            <p className="footer__cate__list">{item.title}</p>
+          ))}
+        </div>
+        <div className="footer__socials">
+          <h2 className="footer__socials__title">Social Media</h2>
+          <div className="footer__socials__icons">
+            {footerSocials.map((item) => (
+              <i className="footer__socials__icon">{item.image}</i>
+            ))}
+          </div>
+        </div>
+      </div>
+      <p className="footer__end">© 2023 GreenShop. Abbos Omonboyev.</p>
     </div>
   );
 };
