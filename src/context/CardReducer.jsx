c8658fcd8fb916b5269b7fb7925da7516c437607
Abbos1506 <abbosomonboyev686@gmail.com>
2023-12-26 20:12:52 +0500

    finish project

--- src/context/CardReducer.jsx
+++ src/context/CardReducer.jsx
@@ -1,22 +1,28 @@
-import { ADD_TO_CART, REMOVE_ITEM } from "./Types";
+import { SHOW_HIDE_CART, ADD_TO_CART, REMOVE_ITEM } from "./Types";
 
 const CardReducer = (state, action) => {
-  switch (action.type) {
-    case ADD_TO_CART: {
-      return {
-        ...state,
-        cardItems: [...state.cardItems, action.payload],
-      };
+    switch (action.type) {
+        case SHOW_HIDE_CART: {
+            return {
+                ...state,
+                showCard: !state.showCard,
+            };
+        }
+        case ADD_TO_CART: {
+            return {
+                ...state,
+                cardItems: [...state.cardItems, action.payload],
+            };
+        }
+        case REMOVE_ITEM: {
+            return {
+                ...state,
+                cardItems: state.cardItems.filter((item) => item.id !== action.payload),
+            };
+        }
+        default: 
+        return state;
     }
-    case REMOVE_ITEM: {
-      return {
-        ...state,
-        cardItems: state.cardItems.filter((item) => item.id !== action.payload),
-      };
-    }
-    default:
-      return state;
-  }
-};
+}
 
 export default CardReducer;
